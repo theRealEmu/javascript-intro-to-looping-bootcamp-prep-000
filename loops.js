ff61563e1c24815cd2336c1953da7e055d0782b0
Emanuel Harms <emanuel.harms@gmx.de>
2018-10-14 10:51:53 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -29,3 +29,9 @@ function whileLoop(number) {
   
 }
 
+
+// TODO: Define a function called doWhileLoop in loops.js. The function should take an array as an argument. Use the incrementVariable() function (you can copy it from this README) as the condition, and remove elements from the array until the array is empty or until incrementVariable() returns false. (Your condition might look something like array.length > 0 && incrementVariable().) Finally, return the array.
+
+function diWhileLoop(array) {
+  
+}
